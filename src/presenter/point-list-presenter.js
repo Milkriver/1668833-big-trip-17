a23b6d808a00dba2e1466fd74f9bcf2df5813ba3
Milkriver <mapav4@yandex.ru>
2022-04-28 12:10:18 +0500

    правки по комментариям наставника

--- src/presenter/point-list-presenter.js
+++ src/presenter/point-list-presenter.js
@@ -1,4 +1,3 @@
-
 import { render } from '../render.js';
 import EditPointView from '../view/edit-point.js';
 import PointView from '../view/point.js';
@@ -7,6 +6,7 @@ export default class PointListPresenter {
   init = (boardContainer) => {
     this.boardContainer = boardContainer;
     render(new EditPointView(), this.boardContainer);
+
     for (let i = 0; i < 3; i++) {
       render(new PointView(), this.boardContainer);
     }
