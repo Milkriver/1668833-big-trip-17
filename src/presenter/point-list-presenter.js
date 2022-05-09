78f129c9d96e60d9a4a63cc6fc8d30d746cdfcee
Milkriver <mapav4@yandex.ru>
2022-05-09 23:22:12 +0500

    добавлены приватные поля

--- src/presenter/point-list-presenter.js
+++ src/presenter/point-list-presenter.js
@@ -3,15 +3,19 @@ import EditPointView from '../view/edit-point.js';
 import PointView from '../view/point.js';
 
 export default class PointListPresenter {
-  init = (boardContainer, pointModel) => {
-    this.boardContainer = boardContainer;
-    this.pointModel = pointModel;
-    this.pointList = [...this.pointModel.getPoints()];
+  #boardContainer = null;
+  #pointModel = null;
+  #pointList = [];
 
-    render(new EditPointView(this.pointList[0]), this.boardContainer);
+  init = (boardContainer, pointModel) => {
+    this.#boardContainer = boardContainer;
+    this.#pointModel = pointModel;
+    this.#pointList = [...this.#pointModel.points];
+
+    render(new EditPointView(this.#pointList[0]), this.#boardContainer);
 
     for (let i = 0; i < 3; i++) {
-      render(new PointView(this.pointList[i]), this.boardContainer);
+      render(new PointView(this.#pointList[i]), this.#boardContainer);
     }
   };
 }
