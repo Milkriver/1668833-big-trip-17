e77e1503a56012b0fc278851f9cd6bbc4e284c0d
Milkriver <mapav4@yandex.ru>
2022-05-10 20:19:16 +0500

    добавлено раскрытие формы редактирования

--- src/main.js
+++ src/main.js
@@ -2,20 +2,15 @@ import PointModel from './model/point-model.js';
 import PointListPresenter from './presenter/point-list-presenter.js';
 import { render, RenderPosition } from './render.js';
 import FilterView from './view/filter.js';
-import PointListView from './view/point-list.js';
-import SortView from './view/sort.js';
 import TripInfoView from './view/trip-info.js';
 
 const siteTripMainElement = document.querySelector('.trip-main');
-const siteFilterElement = document.querySelector('.trip-controls__filters');
-const siteSortElement = document.querySelector('.trip-events');
-const pointModel = new PointModel();
-const pointListPresenter = new PointListPresenter();
-
 render(new TripInfoView(), siteTripMainElement, RenderPosition.AFTERBEGIN);
-render(new FilterView(), siteFilterElement, RenderPosition.BEFOREEND);
-render(new SortView(), siteSortElement);
-render(new PointListView(), siteSortElement);
 
-const siteTripEventsListElement = document.querySelector('.trip-events__list');
-pointListPresenter.init(siteTripEventsListElement, pointModel);
+const siteFilterElement = siteTripMainElement.querySelector('.trip-controls__filters');
+render(new FilterView(), siteFilterElement);
+
+const tripEventsElement = document.querySelector('.trip-events');
+const pointModel = new PointModel();
+const pointListPresenter = new PointListPresenter(tripEventsElement, pointModel);
+pointListPresenter.init();
--- src/presenter/point-list-presenter.js
+++ src/presenter/point-list-presenter.js
@@ -1,21 +1,55 @@
 import { render } from '../render.js';
 import EditPointView from '../view/edit-point.js';
 import PointView from '../view/point.js';
+import SortView from '../view/sort.js';
+import PointListView from '../view/point-list.js';
 
 export default class PointListPresenter {
   #boardContainer = null;
   #pointModel = null;
   #pointList = [];
+  #pointListComponent = new PointListView();
 
-  init = (boardContainer, pointModel) => {
+  constructor(boardContainer, pointModel) {
     this.#boardContainer = boardContainer;
     this.#pointModel = pointModel;
+  }
+
+  #renderPoint = (point) => {
+    const pointComponent = new PointView(point);
+    const pointEditComponent = new EditPointView(point);
+
+    const replacePointToForm = () => {
+      this.#pointListComponent.element.replaceChild(pointEditComponent.element, pointComponent.element);
+    };
+
+    const replaceFormToPoint = () => {
+      this.#pointListComponent.element.replaceChild(pointComponent.element, pointEditComponent.element);
+    };
+
+    pointComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+      replacePointToForm();
+    });
+
+    pointEditComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+      replaceFormToPoint();
+    });
+
+
+    render(pointComponent, this.#pointListComponent.element);
+  };
+
+  #renderBoard = () => {
+    render(new SortView(), this.#boardContainer);
+    render(this.#pointListComponent, this.#boardContainer);
+  };
+
+  init = () => {
     this.#pointList = [...this.#pointModel.points];
 
-    render(new EditPointView(this.#pointList[0]), this.#boardContainer);
-
     for (let i = 0; i < 3; i++) {
-      render(new PointView(this.#pointList[i]), this.#boardContainer);
+      this.#renderPoint(this.#pointList[i]);
     }
+    this.#renderBoard();
   };
 }
