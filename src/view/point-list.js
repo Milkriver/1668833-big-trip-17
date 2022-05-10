39308d4426c069d4d21819cb2bb39cfd65bba50c
Milkriver <mapav4@yandex.ru>
2022-05-10 12:41:39 +0500

    добавлены приватные поля и геттеры

--- src/view/point-list.js
+++ src/view/point-list.js
@@ -6,19 +6,21 @@ const createPointsListTemplate = () => (
 );
 
 export default class PointListView {
-  getTemplate() {
+  #element = null;
+
+  get template() {
     return createPointsListTemplate();
   }
 
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
     }
 
-    return this.element;
+    return this.#element;
   }
 
   removeElement() {
-    this.element = null;
+    this.#element = null;
   }
 }
