25d7ae1ad16b252b226e2471a2f8d4d215bba060
Milkriver <mapav4@yandex.ru>
2022-05-16 20:11:04 +0500

    презентер point.WIP

---
+++ src/presenter/point-presenter.js
@@ -0,0 +1,53 @@
+import { render, replace } from '../framework/render';
+import EditPointView from '../view/edit-point';
+import PointView from '../view/point';
+
+export default class PointPresenter {
+  #point = null;
+  #pointComponent = null;
+  #pointEditComponent = null;
+  #pointListContainer = null;
+
+  constructor(pointListContainer) {
+    this.#pointListContainer = pointListContainer;
+  }
+
+  init = (point) => {
+    this.#point = point;
+
+    this.#pointComponent = new PointView(point);
+    this.#pointEditComponent = new EditPointView(point);
+    // eslint-disable-next-line no-console
+    console.log(this.#handleEditClick);
+
+    this.#pointComponent.setEditClickHandler(this.#handleEditClick);
+    this.#pointEditComponent.setEditClickHandler(this.#handleFormSubmit);
+
+    render(this.#pointComponent, this.#pointEditComponent);
+  };
+
+  #replacePointToForm = () => {
+    replace(this.#pointEditComponent, this.pointComponent);
+    document.addEventListener('keydown', this.#onEscKeyDownHandler);
+  };
+
+  #replaceFormToPoint = () => {
+    replace(this.#pointComponent, this.pointEditComponent);
+    document.removeEventListener('keydown', this.#onEscKeyDownHandler);
+  };
+
+  #onEscKeyDownHandler = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      this.replaceFormToPoint();
+    }
+  };
+
+  #handleEditClick = () => {
+    this.#replacePointToForm();
+  };
+
+  #handleFormSubmit = () => {
+    this.#replaceFormToPoint();
+  };
+}
